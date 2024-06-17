9ce1651475abd1d6146a7c697cb1fd7900cb5ebf
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2024-06-17 15:06:02 +0100

    add loading gif

--- app.py
+++ app.py
@@ -9,6 +9,44 @@ st.set_page_config(layout="wide")
 from pathlib import Path
 results_path = Path('./results/')
 
+def load_map(map_path):
+    custom_html = """
+    <style>
+    #spinner {{
+        z-index: 99999;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(255, 255, 255, 0.8);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }}
+    </style>
+
+    <div id="spinner">
+        <h2>Loading Interactive Map...</h2>
+        <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjA0NGFrZ2h4MWQwaHk0c2I5MXdwajBybWV0aG5qb2ppbmtxenc0OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VI2UC13hwWin1MIfmi/giphy.webp" alt="Loading Interactive Map..." />
+    </div>
+
+    <iframe id="documentIFrame" onload="closeIndicator()" src="{map_path}" style="width: 100%; height: 1000px; border: none;">
+    </iframe>
+
+    <script type="text/javascript">
+    function closeIndicator() {{
+        var s = document.getElementById('spinner');
+        console.log(s);
+        s.style.display = "none";
+    }}
+    </script>
+    """.format(map_path=map_path)  # Replace with your actual map_path
+    return custom_html
+
+
+
 def run():
     models = [f.name for f in results_path.glob('*/') if f.is_dir()]
     models =['Disrupted', 'Undisrupted']
@@ -16,6 +54,7 @@ def run():
     option = option.lower()
 
     st.write(f"Loading {option}")
+
 
     with open(results_path/option/'static.pkl', 'rb') as f:
         r = pickle.load(f)
@@ -48,8 +87,10 @@ def run():
     col4.metric("Solve time", f"{result.solve_time:.2f}s")
     period_s3 =str(period).split(' ')[0]
     map_path =fr'https://trade-optimisation-data-viewer.s3.eu-west-2.amazonaws.com/results/{option.lower()}/html/{period_s3}+00%3A00%3A00.html'
+    print(map_path)
 
-    st.components.v1.iframe(map_path, scrolling=False, height=1000)
+    custom_html = load_map(map_path)
+    components.html(custom_html, height=1000)
     st.plotly_chart(result.supply_fig, use_container_width=True)
 
     st.plotly_chart(result.S_fig, use_container_width=True)
